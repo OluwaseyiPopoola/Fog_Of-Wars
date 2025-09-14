29da43f8fa34edafd6612ee815498880714c352f
Oluwaseyi Popoola <oluwaseyipopoola49@gmail.com>
2025-09-14 15:46:12 -0500

    Add game story introduction and user prompts

--- fog_of_wars.py
+++ fog_of_wars.py
@@ -11,12 +11,25 @@ def main():
     game_name = 'FOG OF WARS'
     display_game_title(game_name)
     print(f"\n{input("Press Enter to start the game...")}")
+    
+    introduce_game_story()
 
 
 def display_game_title(msg):
     f = Figlet(font='rectangles')
     print(f.renderText(msg))
 
+def introduce_game_story():
+
+    print("\nIn a land shrouded by the Fog of War, a lone hero embarks on a quest to vanquish enemies and uncover hidden treasures.")
+    wait_for_user()
+    print("\nArmed with courage and determination, the hero must navigate through treacherous terrains, face formidable foes, ")
+    wait_for_user()
+    print("\nand unlock the secrets of mystical chests that hold the key to ultimate power. Will the hero emerge victorious or succumb to the perils that lie ahead?")
+    wait_for_user()
+
+    def wait_for_user():
+        input("\nPress Enter to continue...")
 
 
 
