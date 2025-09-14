2323eb1f8cbc69daea7f778c879cc041e0cec068
Oluwaseyi Popoola <oluwaseyipopoola49@gmail.com>
2025-09-14 15:37:41 -0500

    Implement main function and display game title

--- fog_of_wars.py
+++ fog_of_wars.py
@@ -1,5 +1,20 @@
+from pyfiglet import Figlet
+
+
+
+
+
+
+
+
 def main():
-    
+    game_name = 'FOG OF WARS'
+    display_game_title(game_name)
+
+
+def display_game_title(msg):
+    f = Figlet(font='rectangles')
+    print(f.renderText(msg))
 
 
 if __name__ == "__main__":
