0689b1c24e2f2b074bb4163c6d03f59ec8be0b52
Oluwaseyi Popoola <oluwaseyipopoola49@gmail.com>
2025-09-14 15:43:10 -0500

    Add prompt to start the game after displaying title

--- fog_of_wars.py
+++ fog_of_wars.py
@@ -10,11 +10,14 @@ from pyfiglet import Figlet
 def main():
     game_name = 'FOG OF WARS'
     display_game_title(game_name)
+    print(f"\n{input("Press Enter to start the game...")}")
 
 
 def display_game_title(msg):
     f = Figlet(font='rectangles')
     print(f.renderText(msg))
+
+
 
 
 if __name__ == "__main__":
